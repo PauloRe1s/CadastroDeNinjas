c49f231af7af5785f9ea4aa94452d10a8a96cad5
PauloRe1s <paullogdx01@gmail.com>
2025-12-31 21:18:10 -0300

    Criado a entidade ninja para persistir no Banco de Dados

--- src/main/java/dev/kisuk1/CadastroDeNinjas/Model/NinjaModel.java
+++ src/main/java/dev/kisuk1/CadastroDeNinjas/Model/NinjaModel.java
@@ -1,4 +1,51 @@
 package dev.kisuk1.CadastroDeNinjas.Model;
 
+import jakarta.persistence.*;
+
+
+//TODO: Entity - Transforma a classe em uma entidade no Banco de Dados (Tabela)
+@Entity
+@Table(name = "tb_cadastro")
 public class NinjaModel {
+
+    //TODO: Atributos - Colunas da tabrla
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    //TODO: NoArgsConstructors
+    public NinjaModel() {}
+
+    //TODO: AllArgsConstructors
+    public NinjaModel(Long id, String nome, String email, int idade) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    //TODO: Getters e Setters
+    public Long getId() { return id; }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() { return idade; }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
 }
