9736619773230a7f2945964e9c834067da38f796
PauloRe1s <paullogdx01@gmail.com>
2026-01-03 19:45:28 -0300

    refactor: Estruturando o projeto para Arquitetura em Camadas

---
+++ src/main/java/dev/kisuk1/CadastroDeNinjas/Repository/MissoesRepository.java
@@ -0,0 +1,7 @@
+package dev.kisuk1.CadastroDeNinjas.Repository;
+
+import dev.kisuk1.CadastroDeNinjas.Model.MissoesModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
+}
