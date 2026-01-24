01f22e852174de38667f85883b52b0768fba620a
PauloRe1s <paullogdx01@gmail.com>
2026-01-23 22:13:13 -0300

    feat: Cria as Regras de Negócio da MissaoService 'CRUD', para serem consumidas pela MissaoController.

---
+++ src/main/java/dev/kisuk1/CadastroDeNinjas/Service/MissaoService.java
@@ -0,0 +1,45 @@
+package dev.kisuk1.CadastroDeNinjas.Service;
+
+import dev.kisuk1.CadastroDeNinjas.Model.MissaoModel;
+import dev.kisuk1.CadastroDeNinjas.Repository.MissaoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MissaoService {
+
+    private MissaoRepository missaoRepository;
+
+    public MissaoService(MissaoRepository missaoRepository) {
+        this.missaoRepository = missaoRepository;
+    }
+
+    public MissaoModel createMissao(MissaoModel newMissao) {
+        return missaoRepository.save(newMissao);
+    }
+
+    public List<MissaoModel> readAllMissao() {
+        return missaoRepository.findAll();
+    }
+
+    public MissaoModel readByIdMissao(Long id) {
+        Optional<MissaoModel> missaoExists  = missaoRepository.findById(id);
+        return missaoExists.orElse(null);
+    }
+
+    public MissaoModel updateMissao(Long id, MissaoModel upMissao) {
+        Optional<MissaoModel> missaoExists = missaoRepository.findById(id);
+        if (missaoExists.isPresent()) {
+            upMissao.setId(id);
+            MissaoModel missaoSave = missaoRepository.save(upMissao);
+            return missaoSave;
+        }
+        return null;
+    }
+
+    public void deleteMissao(Long id) {
+        missaoRepository.deleteById(id);
+    }
+}
