cb5909a36e781ddfa1d795c112aba56cce4a8f6e
PauloRe1s <paullogdx01@gmail.com>
2026-01-25 20:40:39 -0300

    feat: Cria NinjaDTO para tirar a responsabilidade do NinjaModel de interagir diretamente com as Requisições.

--- src/main/java/dev/kisuk1/CadastroDeNinjas/DTO/NinjaDTO.java
+++ src/main/java/dev/kisuk1/CadastroDeNinjas/DTO/NinjaDTO.java
@@ -1,4 +1,21 @@
 package dev.kisuk1.CadastroDeNinjas.DTO;
 
+import dev.kisuk1.CadastroDeNinjas.Model.MissaoModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private String rank;
+    private MissaoModel missao;
+
 }
