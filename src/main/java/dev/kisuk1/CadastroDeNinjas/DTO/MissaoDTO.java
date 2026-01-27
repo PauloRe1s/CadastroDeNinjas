17fab191c4f36faa83a6862c930a897beb0a1ce0
PauloRe1s <paullogdx01@gmail.com>
2026-01-26 21:19:44 -0300

    feat: Cria o MissaoDTO e MissaoMapper.

--- src/main/java/dev/kisuk1/CadastroDeNinjas/DTO/MissaoDTO.java
+++ src/main/java/dev/kisuk1/CadastroDeNinjas/DTO/MissaoDTO.java
@@ -1,4 +1,20 @@
 package dev.kisuk1.CadastroDeNinjas.DTO;
 
+import dev.kisuk1.CadastroDeNinjas.Model.NinjaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissaoDTO {
+
+    private Long id;
+    private String nome;
+    private String dificuldade;
+    private List<NinjaModel> ninjas;
+
 }
--- src/main/java/dev/kisuk1/CadastroDeNinjas/Mapper/MissaoMapper.java
+++ src/main/java/dev/kisuk1/CadastroDeNinjas/Mapper/MissaoMapper.java
@@ -1,4 +1,33 @@
 package dev.kisuk1.CadastroDeNinjas.Mapper;
 
+import dev.kisuk1.CadastroDeNinjas.DTO.MissaoDTO;
+import dev.kisuk1.CadastroDeNinjas.Model.MissaoModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MissaoMapper {
+
+    public MissaoModel map(MissaoDTO missaoDTO) {
+
+        MissaoModel missaoModel = new MissaoModel();
+
+        missaoModel.setId(missaoDTO.getId());
+        missaoModel.setNome(missaoDTO.getNome());
+        missaoModel.setDificuldade(missaoDTO.getDificuldade());
+        missaoModel.setNinjas(missaoDTO.getNinjas());
+
+        return missaoModel;
+    }
+
+    public MissaoDTO map(MissaoModel missaoModel) {
+
+        MissaoDTO missaoDTO = new MissaoDTO();
+
+        missaoDTO.setId(missaoModel.getId());
+        missaoDTO.setNome(missaoModel.getNome());
+        missaoDTO.setDificuldade(missaoModel.getDificuldade());
+        missaoDTO.setNinjas(missaoModel.getNinjas());
+
+        return missaoDTO;
+    }
 }
