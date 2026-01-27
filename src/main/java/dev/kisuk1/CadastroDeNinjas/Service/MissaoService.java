8c8eb93a656b0a2b8f3e731cd017c9142182ebc3
PauloRe1s <paullogdx01@gmail.com>
2026-01-26 21:47:53 -0300

    fix: Altera a lógica de negócio, passando a responsabilidade do MissaoModel para o MissaoDTO.

--- src/main/java/dev/kisuk1/CadastroDeNinjas/Service/MissaoService.java
+++ src/main/java/dev/kisuk1/CadastroDeNinjas/Service/MissaoService.java
@@ -1,39 +1,51 @@
 package dev.kisuk1.CadastroDeNinjas.Service;
 
+import dev.kisuk1.CadastroDeNinjas.DTO.MissaoDTO;
+import dev.kisuk1.CadastroDeNinjas.Mapper.MissaoMapper;
 import dev.kisuk1.CadastroDeNinjas.Model.MissaoModel;
 import dev.kisuk1.CadastroDeNinjas.Repository.MissaoRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class MissaoService {
 
     private MissaoRepository missaoRepository;
+    private MissaoMapper missaoMapper;
 
-    public MissaoService(MissaoRepository missaoRepository) {
+    public MissaoService(MissaoRepository missaoRepository, MissaoMapper missaoMapper) {
         this.missaoRepository = missaoRepository;
+        this.missaoMapper = missaoMapper;
     }
 
-    public MissaoModel createMissao(MissaoModel newMissao) {
-        return missaoRepository.save(newMissao);
+    public MissaoDTO createMissao(MissaoDTO newMissaoDTO) {
+        MissaoModel newMissaoModel = missaoMapper.map(newMissaoDTO);
+        MissaoModel missaoModelSave = missaoRepository.save(newMissaoModel);
+        return missaoMapper.map(missaoModelSave);
     }
 
-    public List<MissaoModel> readAllMissao() {
-        return missaoRepository.findAll();
+    public List<MissaoDTO> readAllMissao() {
+        List<MissaoModel> listMissaoModel = missaoRepository.findAll();
+        return listMissaoModel.stream()
+                .map(missaoMapper::map)
+                .collect(Collectors.toList());
     }
 
-    public MissaoModel readByIdMissao(Long id) {
+    public MissaoDTO readByIdMissao(Long id) {
         Optional<MissaoModel> missaoExists  = missaoRepository.findById(id);
-        return missaoExists.orElse(null);
+        return missaoExists.map(missaoMapper::map).orElse(null);
     }
 
-    public MissaoModel updateMissao(Long id, MissaoModel upMissao) {
+    public MissaoDTO updateMissao(Long id, MissaoDTO upMissaoDTO) {
         Optional<MissaoModel> missaoExists = missaoRepository.findById(id);
         if (missaoExists.isPresent()) {
-            upMissao.setId(id);
-            return missaoRepository.save(upMissao);
+            MissaoModel upMissaoModel = missaoMapper.map(upMissaoDTO);
+            upMissaoDTO.setId(id);
+            MissaoModel missaoModelSave = missaoRepository.save(upMissaoModel);
+            return missaoMapper.map(missaoModelSave);
         }
         return null;
     }
